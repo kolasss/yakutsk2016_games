26c25484fbbbab29007eb1d7b46b09bfc0396f01
kolasss <kolas.batman@gmail.com>
2016-07-04 18:52:37 +0900

    добавил продакшн сервер

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,6 +7,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
+server '62.76.41.79', user: 'kolas', roles: %w{web app db}, port: 54422
 
 
 # role-based syntax
